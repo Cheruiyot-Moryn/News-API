e1fbdd693e2c4411baa852a1b56b8dbb47ede624
Moureen Chepkoech <moureen.chepkoech@student.moringaschool.com>
2022-05-03 14:22:31 +0300

    initializing the application

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+
+
+bootstrap = Bootstrap()
+
+
+
+app = Flask(__name__)
+
+
+
+
+ # Initializing flask extensions
+bootstrap.init_app(app)
