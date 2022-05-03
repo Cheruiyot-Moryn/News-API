c4ec1114f82680d9bd0389faba266bb4cc49282f
Moureen Chepkoech <moureen.chepkoech@student.moringaschool.com>
2022-05-03 14:35:48 +0300

    add function that configures the app request

--- app/request.py
+++ app/request.py
@@ -1,2 +1,15 @@
 import urllib.request,json
 from .models import Article, Category, Source , Headlines
+
+
+
+
+
+
+
+
+def configure_request(app):
+    global api_key, source_url, cat_url
+    api_key = app.config['NEWS_API_KEY']
+    source_url= app.config['NEWS_API_SOURCE_URL']
+    cat_url=app.config['CAT_API_URL']
